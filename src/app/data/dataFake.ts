416015d71f505b6c6bc274674be9c28c86bb8e04
Wagner Morais <moraiswg@hotmail.com>
2023-10-21 16:32:49 -0300

    criando compontentes dinamicos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "A mensagem da Restauração do evangelho de Jesus Cristo",
+    "description": "Deus é nosso Pai Celestial. Somos Seus filhos. Ele tem um corpo de carne e ossos que é glorificado e aperfeiçoado. Ele nos ama. Ele entende e sente nossas tristezas e Se regozija com nosso progresso. Ele deseja Se comunicar conosco, e podemos nos comunicar com Ele por meio de uma oração sincera.",
+    "photoCover": "https://assets.churchofjesuschrist.org/4d/25/4d25ab0daaa200491816a04e971f430a60274b38/4d25ab0daaa200491816a04e971f430a60274b38.jpeg"
+  },
+  {
+    "id":"2",
+    "title":"O plano de salvação. Vida pré-mortal: O propósito e o plano de Deus para nós. Muitas pessoas se perguntam: “De onde viemos? Por que estamos aqui? Para onde iremos?” O plano de salvação fornece respostas a essas perguntas.",
+    "description":"ola",
+    "photoCover":"https://i.pinimg.com/originals/74/68/b4/7468b4ca91edbbaafcc65ada3ee562a4.jpg"
+  },
+  
+]
